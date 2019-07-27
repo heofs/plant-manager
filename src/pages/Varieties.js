aa216df4bf951bb3053263a5e615172ef1ac50e9
Henning Ofstad <ofstad10@gmail.com>
2019-07-27 14:46:38 +0100

    Add apollo graphql client

--- src/pages/Varieties.js
+++ src/pages/Varieties.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import { Button } from 'reactstrap';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  headers: {
+    authorization: 'testTokenCeyJhbGciOiJSU',
+  },
+});
+
+const handleQuery = () => {
+  client
+    .query({
+      query: gql`
+        {
+          allVarieties {
+            id
+            variety
+          }
+        }
+      `,
+    })
+    .then(result => console.log(result));
+};
 
 const VarietiesPage = () => {
+  handleQuery();
   return (
     <div>
       <h1>Varieties</h1>
+      <Button onClick={() => handleQuery()}>Query</Button>
     </div>
   );
 };
