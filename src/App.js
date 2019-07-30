4abae63492e7dd7232233ad29908c5ad3348218b
Henning Ofstad <ofstad10@gmail.com>
2019-07-30 11:51:36 +0100

    Move apollo client to top level

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
@@ -17,19 +19,28 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 library.add(faHeart, faLeaf, faSeedling, faBraille, faPray);
 
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  headers: {
+    authorization: 'testTokenCeyJhbGciOiJSU',
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Dashboard} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/plants" component={Plants} />
-          <Route path="/varieties" component={Varieties} />
-          <Route path="*" exact={true} component={PageNotFound} />
-        </Switch>
-      </Layout>
-    </Router>
+    <ApolloProvider client={apolloClient}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/plants" component={Plants} />
+            <Route path="/varieties" component={Varieties} />
+            <Route path="*" exact={true} component={PageNotFound} />
+          </Switch>
+        </Layout>
+      </Router>
+    </ApolloProvider>
   );
 }
 
