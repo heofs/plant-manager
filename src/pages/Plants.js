529b848238bcd6609cc932d50491aaebad8c9578
Henning Ofstad <ofstad10@gmail.com>
2019-08-16 17:03:46 +0100

    add firebase to context

--- src/pages/Plants.js
+++ src/pages/Plants.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { withFirebaseUser } from '../components/Firebase';
 
-const PlantsPage = () => {
-  return (
-    <div>
-      <h1>Plants</h1>
-    </div>
-  );
-};
+class PlantsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.user = this.props.firebase.currentUser();
+  }
+  logUser() {
+    console.log(this.user);
+  }
+  componentDidMount() {
+    this.props.firebase.currentUser().onAuthStateChanged(user => {
+      if (user) {
+        console.log('Got user');
+        console.log(user.ra);
+      }
+    });
 
-export default PlantsPage;
+    // this.props.firebase
+    //   .signIn('ofstad10@gmail.com', 'Password123')
+    //   .then(msg => {
+    //     console.log(msg);
+    //   });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 onClick={() => this.logUser()}>Plants</h1>
+      </div>
+    );
+  }
+}
+
+export default withFirebaseUser(PlantsPage);
