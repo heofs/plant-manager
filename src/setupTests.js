77a2b61e8470486c501e4e9bdac2bc6d252323f1
Henning Ofstad <ofstad10@gmail.com>
2019-08-20 21:04:59 +0100

    Remove obscolete import

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,5 @@
 import 'jest-styled-components';
 
-import '@testing-library/react/cleanup-after-each';
 import '@testing-library/jest-dom/extend-expect';
 import { configure } from '@testing-library/dom';
 
