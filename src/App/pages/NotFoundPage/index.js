76b08400177522244cea740b51845a437aab5bf2
Henning Ofstad <ofstad10@gmail.com>
2019-08-23 22:45:39 +0100

    Update 404 page

--- src/App/pages/NotFoundPage/index.js
+++ src/App/pages/NotFoundPage/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+import errorImage from 'images/error-404.svg';
+
+const Wrapper = styled.div`
+  margin-top: 5em;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+`;
 
 const NotFoundPage = ({ history }) => {
   if (history && history.location.pathname === '/login') {
     return <Redirect to={'/'} />;
   }
-  if (history && history.location.pathname !== '/404') {
-    history.push('/404');
-  }
   return (
-    <div>
-      <h1>404 - The page was not found.</h1>
+    <Wrapper>
+      <img src={errorImage} alt="404" height={'200px'} />
+      <h1>Sorry, the page was not found</h1>
+      <p>
+        Route{' '}
+        <span className="font-weight-bold">{history.location.pathname}</span>{' '}
+        was not found.
+      </p>
       <h2>
         <Link to={'/'}>Go back to frontpage</Link>
       </h2>
-    </div>
+    </Wrapper>
   );
 };
 
